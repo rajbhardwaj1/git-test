d0075c18cc24ca631a49d1b22dc94115aab34bd9
Raj <rajbhardwaj.bgs@gmail.com>
2024-09-06 21:04:36 +0530

    react-app

---
+++ react1/src/abc.jsx
@@ -0,0 +1,6 @@
+function Abc() {
+    return(
+        <h1>abc (another file)</h1>
+    );
+}
+export default Abc
---
+++ test.js
@@ -0,0 +1,13 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+console.log(reverseString("olleh"));
+
+
+function rev(str){
+    return str.split('').reverse().join('');    
+}
+console.log(rev('ramuk jar'));
+
+
+  
